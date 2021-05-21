61a4e6de94147e56a91f3be644cbd0207b9ad3b1
valeriavalrosso <81103827+valeriavalrosso@users.noreply.github.com>
2021-05-21 14:14:49 +0200

    Aggiornamento OpuscoloInformativo

--- app/Views/opuscolo.php
+++ app/Views/opuscolo.php
@@ -6,24 +6,26 @@
 	<title>Opuscolo Informativo</title>
 	<style>
 	.div-opuscolo {
-	  background-color: lightgrey;
+	  background-color: #3297d340;
 	  border-radius: 20px;
 	  padding-top: 10px;
 	  padding-bottom: 10px;
 	  padding-left: 20px;
 	  padding-right: 20px;
 	  margin: 2%;
-	  font-family: sans-serif;
+	  font-family: Open Sans, sans-serif;
 	  color: black;
 	}
 	
 	.p-opuscolo {
-	  font-size: 25px;
+	  font-size: 22px;
 	  color: black;
 	}
 	
 	.h1-opuscolo {
 	  color: black;
+	  margin-top: 50;
+	  margin-bottom: -6;
 	}
 	</style>
 </head>
@@ -33,46 +35,46 @@
 		<?php include 'common/navbar.php';?>
 	</div>
 
-	<h1 class="h1-opuscolo" style='text-align:center';>Opuscolo Informativo</h1></br></br>
+	<h1 class="h1-opuscolo" style='text-align:center';><b>Opuscolo Informativo</br></h1></br></br>
 
 	<div class="div-opuscolo">
 		<h1 class="h1-opuscolo"><i>Quante e quali sono le tipologie di tampone?</i></h1>
-		<p  class="p-opuscolo" style="margin-top: 2%">Le tipologie di tampone sono 2:</br></p>
-		<p  class="p-opuscolo" style="margin-left: 1% ; margin-top: 2%">
-		TAMPONE MOLECOLARE: è il test attualmente più affidabile per la diagnosi di infezione da coronavirus. Viene eseguito su un campione prelevato con un tampone a livello naso/oro-faringeo, 
+		<p  class="p-opuscolo" style="margin-left: 1%; margin-top: 2%">Le tipologie di tampone sono due:</br></p>
+		<ul><p  class="p-opuscolo" style="margin-left: 1% ; margin-top: 2%">
+		- TAMPONE MOLECOLARE: è il test attualmente più affidabile per la diagnosi di infezione da coronavirus. Viene eseguito su un campione prelevato con un tampone a livello naso/oro-faringeo, 
 		e quindi analizzato attraverso metodi molecolari di real-time RT-PCR (Reverse Transcription-Polymerase Chain Reaction) per l’amplificazione dei geni virali maggiormente espressi durante 
 		l’infezione. L’analisi può essere effettuata solo in laboratori altamente specializzati, individuati dalle autorità sanitarie, e richiede in media dalle 2 alle 6 ore dal momento in cui 
 		il campione viene avviato alla processazione in laboratorio;</br></br>
-		TAMPONE ANTIGENICO (RAPIDO): questa tipologia di test è basata sulla ricerca, nei campioni respiratori, di proteine virali (antigeni). Le modalità di raccolta del campione sono del 
+		- TAMPONE ANTIGENICO (RAPIDO): questa tipologia di test è basata sulla ricerca, nei campioni respiratori, di proteine virali (antigeni). Le modalità di raccolta del campione sono del 
 		tutto analoghe a quelle dei test molecolari (tampone naso-faringeo), i tempi di risposta sono molto brevi (circa 15 minuti), ma la sensibilità e specificità di questo test sembrano 
 		essere inferiori a quelle del test molecolare. Ciò comporta la possibilità di risultati falso-negativi in presenza di bassa carica virale (tC>25), oltre alla necessità di confermare i 
-		risultati positivi mediante un tampone molecolare.</p>
+		risultati positivi mediante un tampone molecolare.</p></ul>
 	</div>
 	
 	<div class="div-opuscolo">
 		<h1 class="h1-opuscolo"><i>Cosa devo fare prima di effettuare il tampone?</i></h1>
 		<p  class="p-opuscolo" style="margin-top: 2%">Prima di effettuare il tampone è necessario compilare il consenso informato e il questionario di anamnesi.  Per ragioni di validità la compilazione deve 
 		avvenire al momento dell'esame diagnostico, pertanto all'utente registrato al sistema verrà fornito, dal laboratorio, un codice di accesso alla sezione del questionario, per poterlo 
-		compilare online. </br></br>E' possibile visualizzare un prototipo dei documenti qui di seguito:</br>
+		compilare online. </br></br>E' possibile visualizzare un prototipo dei documenti qui di seguito:</br></br>
 		<a href="docs/Consenso test antigenico compilabile.pdf" download>Consenso Informato</a></br>
 		<a href="docs/Questionario valutazione del rischio da contagio COVID-19 270121_compilabile.pdf" download>Questionario di Anamnesi</a></p>
 	</div>
 	
 	<div class="div-opuscolo">
 		<h1 class="h1-opuscolo"><i>Come devo comportarmi se il risultato del mio test è negativo?</i></h1>
-		<p  class="p-opuscolo" style="margin-top: 2%">Esiste la possibilità che nonostante la negatività del test sia stata comunque contratta l'infezione da SARS-CoV-2, e che si manifestino sintomi nei giorni seguenti. 
-		Il periodo più lungo di incubazione della malattia da COVID-19 descritto è di 21 giorni. Si deve pertanto monitorare l'eventuale insorgenza di sintomi e ad applicare le misure preventive 
-		previste per la prevenzione del contagio, ovvero l'utilizzo della mascherina, la disinfezione frequente delle mani, l'areazione dei locali dove si soggiorna e/o lavora. L’infezione dà 
+		<p  class="p-opuscolo" style="margin-top: 2%">Esiste la possibilità che, nonostante la negatività del test, sia stata comunque contratta l'infezione da SARS-CoV-2 e che si manifestino sintomi nei giorni seguenti. 
+		Il periodo più lungo di incubazione, della malattia da COVID-19, descritto è di 21 giorni. Si deve pertanto monitorare l'eventuale insorgenza di sintomi e applicare le misure
+		previste per la prevenzione del contagio: l'utilizzo della mascherina, la disinfezione frequente delle mani, l'areazione dei locali dove si soggiorna e/o lavora. L’infezione dà 
 		SARS-CoV-2 porta a una malattia la cui presentazione è proteiforme, variando da inapparente a una sintomatologia simile a quella di un raffreddore comune, fino alla polmonite con grave 
 		difficoltà respiratoria, potenzialmente fatale. Si può sospettare un’infezione da SARS-CoV-2 se il quadro sindromico iniziale comprende febbre con andamento continuo o intermittente, 
 		tosse secca, astenia, mialgie e artralgie e disturbi gastrointestinali. I sintomi non respiratori possono precedere quelli respiratori e la febbre. Nella forma di malattia lieve, in 
 		cui non c’è polmonite o ipossia, la febbre dura poco o manca e sono prevalenti il mal di testa e di gola, la congestione nasale, la tosse e la perdita dell’olfatto. A volte vi sono 
 		diarrea, nausea o vomito, spossatezza, mancanza di appetito, modificazioni cutanee specie alle estremità, perdita del gusto e/o dell’olfatto.</p>
-		<h1 class="h1-opuscolo" style="margin-top: 2%"><i>E come invece se è positivo?</i></h1>
-		<p  class="p-opuscolo">Se il risultato del test è positivo la prima cosa da fare è isolarsi dal resto della famiglia, se possibile in una stanza singola ben ventilata, evitare contatti sociali e 
+		<h1 class="h1-opuscolo" style="margin-top: 2%"><br><i>E come invece se è positivo?</br></i></h1>
+		<p  class="p-opuscolo" style="margin-top: 2%">Se il risultato del test è positivo, la prima cosa da fare è isolarsi dal resto della famiglia, se possibile in una stanza singola ben ventilata, evitare contatti sociali e 
 		comunicare la propria positività al proprio Medico di Medicina Generale. Inoltre, preparare l’elenco delle persone con cui si ha avuto contatti a rischio nei due giorni precedenti 
-		l’inizio dei sintomi (o la data di effettuazione del tampone se non ha sintomi), poichè sarà utile al Dipartimento di Prevenzione. Pertanto, previa informativa al proprio medico di medicina 
-		generale, si dovrà rimanere in quarantena obbligatoria, nel rispetto degli adempimenti previsti dalle normative vigenti.
-		NB: Gli individui risultati positivi potranno uscire dalla quarantena solo dopo essere stati sottoposti all’esecuzione di un tampone che sia risultato negativo (guarigione clinica e virologica).</p>
+		l’inizio dei sintomi (o la data di effettuazione del tampone se non si hanno sintomi), poichè sarà utile al Dipartimento di Prevenzione. Pertanto, previa informativa al proprio medico di medicina 
+		generale, si dovrà rimanere in quarantena obbligatoria, nel rispetto degli adempimenti previsti dalle normative vigenti.<br>
+		N.B. Gli individui risultati positivi potranno uscire dalla quarantena solo dopo essere stati sottoposti all’esecuzione di un tampone che sia risultato negativo (guarigione clinica e virologica).</br></p>
 	</div>
 </body>
